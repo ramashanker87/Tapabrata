abd4dce803c4eeee276bccd817d15e80b698bb06
Tapabrata <tapabrata@abc.com>
2025-01-28 20:10:05 -0800

    Day07 Commit

---
+++ Day07/spring-student-app/src/main/java/com/springboot/spring_student_app/SpringStudentAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.spring_student_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudentAppApplication.class, args);
+	}
+
+}
---
+++ Day07/spring-student-app/src/test/java/com/springboot/spring_student_app/SpringStudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.spring_student_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
